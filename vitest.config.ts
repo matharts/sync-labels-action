737aa96e7b2f0ed9e293680bd6dafc5e519d1ea2
周行子 <lzm0x219@gmail.com>
2026-07-15 17:12:01 +0800

    ci: upload test results to Codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    reporters: ["default", "junit"],
+    outputFile: {
+      junit: "./test-results/junit.xml",
+    },
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
